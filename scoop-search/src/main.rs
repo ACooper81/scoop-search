87fdd3c9b424f1e6f3c2e670fe1d86d3b5b23c8c
Adrian Cooper <adriancooper81@hotmail.com>
2023-04-22 23:55:01 +0930

    Push

--- scoop-search/src/main.rs
+++ scoop-search/src/main.rs
@@ -1,22 +1,5 @@
 use serde_json::{Result, Value};
-
-fn search_query(v: &mut Vec<Manifest>, query: &str) -> Result<()>{
-    let input_path: String = "C:\\Users\\Adrian\\scoop\\buckets\\Scoop-Apps\\bucket\\SUMo-Portable.json".to_string();
-    let text = std::fs::read_to_string(&input_path).unwrap();
-    let val: Value = serde_json::from_str(&text).unwrap();
-
-    if query == "" {
-        
-    } else {
-        v.push(Manifest {
-            name: val["name"].to_string(), 
-            version: val["version"].to_string().replace("\"", ""), 
-            source: val["source"].to_string(), 
-            binaries: val["bin"].to_string().replace("\"", ""),
-        });
-    }
-    Ok(())
-}
+use std::{env, fs, path};
 
 #[derive(Debug)]
 struct Manifest {
@@ -26,29 +9,15 @@ struct Manifest {
     binaries: String,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let mut _name_count: u8 = 45;
     let mut _version_count: u8 = 20;
     let mut _source_count: u8  = 20;
     let mut _binaries_count: u8 = 25;
     let mut v: Vec<Manifest> = vec![];
-    let query: String = std::env::args().nth(0).unwrap();
-
-    
-    // let input_path: String = "C:\\Users\\Adrian\\scoop\\buckets\\Scoop-Apps\\bucket\\SUMo-Portable.json".to_string();
-    // let text = std::fs::read_to_string(&input_path).unwrap();
-    // let val: Value = serde_json::from_str(&text).unwrap();
-
-    // if query == "" {
-        
-    // } else {
-    //     v.push(Manifest {
-    //         name: val["name"].to_string(), 
-    //         version: val["version"].to_string().replace("\"", ""), 
-    //         source: val["source"].to_string(), 
-    //         binaries: val["bin"].to_string().replace("\"", ""),
-    //     });
-    // }
+    let query = "".to_string();
+    // let query: String = std::env::args().nth(1).unwrap();
+    // let query: String = "sumo".to_string();
 
     v.push(Manifest {
         name: "Name".to_string(), 
@@ -69,6 +38,31 @@ fn main() {
     for m in &v {
         println!("{: <45} {: <20} {: <20} {: <25}", m.name, m.version, m.source, m.binaries);
     }
-    
-    // untyped_example().unwrap();
+
+    // println!("{}", query);
+
+    Ok(())
+}
+
+fn search_query(v: &mut Vec<Manifest>, query: &str) -> Result<()>{
+    let input_path: String = "C:/Users/Adrian/scoop/buckets/Scoop-Apps/bucket/SUMo-Portable.json".to_string();
+    let text = fs::read_to_string(&input_path).unwrap();
+    let val: Value = serde_json::from_str(&text).unwrap();
+
+    let file_stem = path::Path::new(&input_path).file_stem().unwrap().to_str();
+    let file_name = &file_stem.expect("no file stem found").to_string();
+    // println!("{}", &file_stem.expect("no file stem found").to_string());
+    if query != "" {
+        // if val["bin"] {
+
+        // }
+    } else {
+        v.push(Manifest {
+            name: path::Path::new(&input_path).file_stem().unwrap().to_str().expect("no file stem found").to_string(), 
+            version: val["version"].to_string().replace("\"", ""), 
+            source: val["source"].to_string(), 
+            binaries: val["bin"].to_string().replace("\"", ""),
+        });
+    }
+    Ok(())
 }
