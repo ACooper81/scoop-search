5c7bf5a771d5de9b1dbf06e1d8bd81d922905a66
Adrian Cooper <adriancooper81@hotmail.com>
2023-04-29 12:02:17 +0930

    Push

--- target/debug/deps/kernel32-5d81695f6eff6a4b.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Adrian\Documents\GitHub\scoop-search\target\debug\deps\kernel32-5d81695f6eff6a4b.rmeta: C:\Users\Adrian\scoop\persist\rustup-msvc\.cargo\registry\src\github.com-1ecc6299db9ec823\kernel32-sys-0.2.2\src\lib.rs
-
-c:\Users\Adrian\Documents\GitHub\scoop-search\target\debug\deps\kernel32-5d81695f6eff6a4b.d: C:\Users\Adrian\scoop\persist\rustup-msvc\.cargo\registry\src\github.com-1ecc6299db9ec823\kernel32-sys-0.2.2\src\lib.rs
-
-C:\Users\Adrian\scoop\persist\rustup-msvc\.cargo\registry\src\github.com-1ecc6299db9ec823\kernel32-sys-0.2.2\src\lib.rs:
