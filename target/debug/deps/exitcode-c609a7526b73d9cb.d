d1d5cb8327537c273cccec82a11e9562af63ad36
Adrian Cooper <adriancooper81@hotmail.com>
2023-04-29 12:01:35 +0930

    Push

---
+++ target/debug/deps/exitcode-c609a7526b73d9cb.d
@@ -0,0 +1,5 @@
+c:\Users\Adrian\Documents\GitHub\scoop-search\target\debug\deps\exitcode-c609a7526b73d9cb.rmeta: C:\Users\Adrian\scoop\persist\rustup-msvc\.cargo\registry\src\github.com-1ecc6299db9ec823\exitcode-1.1.2\src\lib.rs
+
+c:\Users\Adrian\Documents\GitHub\scoop-search\target\debug\deps\exitcode-c609a7526b73d9cb.d: C:\Users\Adrian\scoop\persist\rustup-msvc\.cargo\registry\src\github.com-1ecc6299db9ec823\exitcode-1.1.2\src\lib.rs
+
+C:\Users\Adrian\scoop\persist\rustup-msvc\.cargo\registry\src\github.com-1ecc6299db9ec823\exitcode-1.1.2\src\lib.rs:
---
+++ target/debug/deps/scoop_search-46db2e3bcc0920ff.d
@@ -0,0 +1,5 @@
+c:\Users\Adrian\Documents\GitHub\scoop-search\target\debug\deps\scoop_search-46db2e3bcc0920ff.rmeta: src\main.rs
+
+c:\Users\Adrian\Documents\GitHub\scoop-search\target\debug\deps\scoop_search-46db2e3bcc0920ff.d: src\main.rs
+
+src\main.rs:
